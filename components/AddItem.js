0fa61efeb617890febb06e5880712d50981be35d
AVINASH KUMAR <Aviproo>
2024-01-03 18:22:52 +0530

    Time to save the TODOs

--- components/AddItem.js
+++ components/AddItem.js
@@ -4,14 +4,29 @@ import classs from "./AddItem.module.css";
 function AddItem(props) {
   const [workData, setWorkData] = useState([]);
   const workRef = useRef();
-  function addWork() {
-    // const work = workRef.current.value;
-    setWorkData([...workData, workRef.current.value]);
+  async function addWork() {
+    const work = workRef.current.value;
+    setWorkData([...workData, work]);
+
+    const todoWork = {
+      work: workRef.current.value,
+    };
+
+    const response = await fetch("/api/hello", {
+      method: "POST",
+      body: JSON.stringify(todoWork),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    const data = await response.json();
+    console.log(data);
   }
   return (
     <Fragment>
       <div className={classs.addItem}>
-        <h1>Add item to Do {props.work}</h1>
+        <h1>Add item to Do </h1>
         <div>
           <input placeholder="Gym" ref={workRef} />
           <button onClick={addWork}>Add Work</button>
@@ -19,12 +34,34 @@ function AddItem(props) {
       </div>
       <div>
         {workData.map((item) => (
-          <div className={classs.item} key={Math.random()}>
+          <div key={Math.random()} className={props.todoList}>
             {item} <button>Delete</button>
           </div>
         ))}
       </div>
     </Fragment>
   );
+}
+
+export async function getStaticProps() {
+  const client = await MongoClient.connect(
+    "mongodb+srv://meetups:FmXGZut5QaWfizAc@cluster0.ufkejyx.mongodb.net/?retryWrites=true&w=majority"
+  );
+  const db = client.db();
+
+  const toDoCollection = db.collection("todoWork");
+
+  const todoList = await toDoCollection.find().toArray();
+
+  client.close();
+  return {
+    props: {
+      todoList: todoList.map((doItem) => ({
+        work: doItem.work,
+        id: doItem._id.toString(),
+      })),
+    },
+    revalidate: 1,
+  };
 }
 export default AddItem;
--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,20 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+import { MongoClient } from "mongodb";
+
+export default async function handler(req, res) {
+  if (req.method === "POST") {
+    const data = req.body;
+
+    const client = await MongoClient.connect(
+      "mongodb+srv://meetups:FmXGZut5QaWfizAc@cluster0.ufkejyx.mongodb.net/?retryWrites=true&w=majority"
+    );
+    const db = client.db();
+    const toDoCollection = db.collection("todoWork");
+
+    const result = await toDoCollection.insertOne(data);
+
+    client.close();
+    res.status(201).json({ message: "to do inserted!" });
+  }
 }
